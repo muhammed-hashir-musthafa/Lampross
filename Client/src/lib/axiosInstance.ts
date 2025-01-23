e1622c28b6f5e5c933dec10a7fc56691b7999a7c
HashirMusthafa <hashirvaliyil8146@gmail.com>
2025-01-24 01:23:12 +0530

    Feat: added backend deployed link

--- Client/src/lib/axiosInstance.ts
+++ Client/src/lib/axiosInstance.ts
@@ -1,11 +1,12 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001/api",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "https://lampross.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
   timeout: 10000,
 });
+console.log(process.env.NEXT_PUBLIC_API_URL)
 
 export default axiosInstance;
